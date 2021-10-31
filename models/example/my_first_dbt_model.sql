a239da2245391bdaa14100307a8e74e247124221
devrajgurjar719@gmail.com <devrajgurjar719@gmail.com>
2021-10-31 08:15:14 +0000

    Removed the default Sample code

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -7,44 +7,24 @@
     Try changing "table" to "view" below
 */
 
-{{ config(materialized='Table') }}
+Create table Country_Info as select distinct country, Continent from "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_DEVRAJ_GURJAR"
 
-with source_data as (
+Create Table Location_Info as SELECT Location , Location_iso_code, Island, Latitude, Longitude From  "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_DEVRAJ_GURJAR" Where Location_level = 'Province'
 
-    select 1 as id
-    union all
-    select null as id
+Create Table Country_wise_cases as SELECT DAte, Total_Recovered, Total_Cases_per_million, Total_active_cases, New_Cases, New_Deaths  From  "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_DEVRAJ_GURJAR" Where Location_level = 'Country'
 
-)
-
-select *
-from source_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
+Create table Province_wise_cases as SELECT Date, LOCATION_ISO_CODE ,Total_Recovered, Total_Cases_per_million, Total_active_cases, New_Cases, New_Deaths  From  "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_DEVRAJ_GURJAR" Where Location_level = 'Province'
 
 
---Create table Country_Info as select distinct country, Continent from "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_DEVRAJ_GURJAR"
+Create table Country_info as SELECT Distinct Country, Continent, LOCATION_ISO_CODE From  "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_DEVRAJ_GURJAR" Where LOCATION_ISO_CODE = 'IDN'
 
---Create Table Location_Info as SELECT Location , Location_iso_code, Island, Latitude, Longitude From  "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_DEVRAJ_GURJAR" Where Location_level = 'Province'
+CREATE TABLE Location_Wise_general_data as SELECT TOTAL_Regencies, POPULATION_DENSITY, POPULATION, TOTAL_CITIES, TOTAL_DISTRICTS, TOTAL_URBAN_VILLAGES, AREA_KM_2_, LOCATION_ISO_CODE
+from "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_DEVRAJ_GURJAR"
 
---Create Table Country_wise_cases as SELECT DAte, Total_Recovered, Total_Cases_per_million, Total_active_cases, New_Cases, New_Deaths  From  "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_DEVRAJ_GURJAR" Where Location_level = 'Country'
-
---Create table Province_wise_cases as SELECT Date, LOCATION_ISO_CODE ,Total_Recovered, Total_Cases_per_million, Total_active_cases, New_Cases, New_Deaths  From  "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_DEVRAJ_GURJAR" Where Location_level = 'Province'
-
-
----Create table Country_info as SELECT Distinct Country, Continent, LOCATION_ISO_CODE From  "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_DEVRAJ_GURJAR" Where LOCATION_ISO_CODE = 'IDN'
-
---CREATE TABLE Location_Wise_general_data as SELECT TOTAL_Regencies, POPULATION_DENSITY, POPULATION, TOTAL_CITIES, TOTAL_DISTRICTS, TOTAL_URBAN_VILLAGES, AREA_KM_2_, LOCATION_ISO_CODE
----from "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_DEVRAJ_GURJAR"
-
---CREATE TABLE Location_Wise_Covid_Stats as 
---SELECT TOTAL_CASES_PER_MILLION, GROWTH_FACTOR_OF_NEW_DEATHS, NEW_CASES_PER_MILLION, NEW_DEATHS_PER_MILLION, CASE_FATALITY_RATE, CASE_RECOVERED_RATE,
---TOTAL_DEATHS_PER_MILLION, GROWTH_FACTOR_OF_NEW_CASES, LOCATION_ISO_CODE
---from "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_DEVRAJ_GURJAR"
+CREATE TABLE Location_Wise_Covid_Stats as 
+SELECT TOTAL_CASES_PER_MILLION, GROWTH_FACTOR_OF_NEW_DEATHS, NEW_CASES_PER_MILLION, NEW_DEATHS_PER_MILLION, CASE_FATALITY_RATE, CASE_RECOVERED_RATE,
+TOTAL_DEATHS_PER_MILLION, GROWTH_FACTOR_OF_NEW_CASES, LOCATION_ISO_CODE
+from "FIVETRAN_INTERVIEW_DB"."GOOGLE_SHEETS"."COVID_19_INDONESIA_DEVRAJ_GURJAR"
 
 Create table Final_Denormalized_Data as 
     select 
